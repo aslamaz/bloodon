337e0fe8514bbf51840ff97df96ed64906afb9b7
Aslam Azees <muhammedazeez473@gmail.com>
2021-08-31 23:59:23 +0530

    env changes

--- src/bloodon/ui/DB.java
+++ src/bloodon/ui/DB.java
@@ -14,8 +14,18 @@ import java.sql.SQLException;
  * @author BlackPearl
  */
 public class DB {
+//   public static Connection getConnection() throws SQLException{
+//        Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/bloodon","root","root");
+//        return con;
+//   }
    public static Connection getConnection() throws SQLException{
-        Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/bloodon","root","root");
+        String host = System.getenv("DB_HOST");
+        String port = System.getenv("DB_PORT");
+        String name = System.getenv("DB_NAME");
+        String username = System.getenv("DB_USER");
+        String password = System.getenv("DB_PASS");
+        String connStr =  String.format("jdbc:mysql://%s:%s/%s", host, port, name);
+        Connection con=DriverManager.getConnection(connStr,username,password);
         return con;
    }
 }
